b55af526ecfde75c05bd211b529bbefdd37940b4
Italo Souza Melo <italosm29@gmail.com>
2023-03-07 16:57:19 -0300

    createUsers migration att

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,9 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import 'reflect-metadata';
 import { app } from './app';
 import { dataSource } from '@shared/infra/typeorm';
 
-dataSource.initialize().then(() => {
-  const server = app.listen(3333, () => {
-    console.log('Server started on port 3333!');
+dataSource
+  .initialize()
+  .then(() => {
+    const server = app.listen(3333, () => {
+      console.log('Server started on port 3333!');
+    });
+  })
+  .catch((err: Error) => {
+    console.log(err);
   });
-});
--- src/shared/infra/typeorm/migrations/1678152642252-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1678152642252-CreateUsers.ts
@@ -7,16 +7,56 @@ export class CreateUsers1678152642252 implements MigrationInterface {
         name: 'users',
         columns: [
           {
-            name: 'id',
+            name: 'userId',
             type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'passwordHash',
+            type: 'varchar',
+          },
+          {
+            name: 'status',
+            type: 'boolean',
+            default: true,
+          },
+          {
+            name: 'role',
+            type: 'enum',
+            enum: [
+              'admin',
+              'user',
+              'seller',
+              'sac',
+              'billing',
+              'manager',
+              'external',
+            ],
+            default: 'user',
+          },
+          {
+            name: 'createdAt',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+          {
+            name: 'updatedAt',
+            type: 'timestamp with time zone',
+            default: 'now()',
           },
         ],
       }),
     );
   }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {}
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+  }
 }
